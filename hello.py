10fe86171b61632f292172a4801e6e1d9725f6de
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-26 08:43:43 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,142 @@
+# import os
+
+# # import something...
+
+
+# print("Hello World...")
+# a = 50
+# b = a
+# print(a)
+# print(b)
+
+
+# print('''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.''')
+
+
+
+
+# for playing sound...
+
+# from playsound import playsound
+# playsound('/home/akhil/Documents/Python/maa.mp3')
+
+
+
+
+# import os
+# print(os.listdir())   #It will show the list of items in dir...
+
+
+
+
+# Variable...
+
+# b = '''Akhil'''
+# c = None
+# d = True
+# e = 45.24
+# f = 234
+
+
+
+# printing variables...
+
+# print(b) 
+# print(c) 
+# print(d) 
+# print(e) 
+# print(f)
+
+
+
+# Printing the data type of variables...
+
+# print(type(b))
+# print(type(c)) 
+# print(type(d)) 
+# print(type(e)) 
+# print(type(f))
+
+
+
+# Operators..
+
+# a = 23
+# b = 7
+# print("Value of a+b is: ", a+b)
+
+# a -= 3  # a = a + 7
+# print(a) 
+
+
+# A = (14 != 3)   # It returns boolean value...
+# print(A)
+
+# bool1 = True
+# bool2 = False
+# print("The value of bool1 and bool2 is: ", (bool1 and bool2))
+# print("The value of bool1 or bool2 is: ", (bool1 or bool2))
+# print("The value of not bool2 is: ", (not bool2))
+
+
+
+
+
+# Type casting and type()
+
+# a = "23440"
+# a = int(a) #String to int
+# print(a + 5)
+# b = 123
+# b = str(b) #int to string
+# print(b)
+# c = 123
+# c = float(c) #int to float
+# print(c)
+# d = 34.56
+# d = int(d)
+# print(d)
+
+
+
+
+
+# Input function
+
+# value = input("Enter your value: ")
+# value = int(value)   #Convert a to integer
+# print(type(value))
+# print("The value is:", value)
+
+
+
+
+# To print remainder... 
+
+# a = 19
+# b = 2
+# c = a % b
+# print("the remainder when a is divided by b is:", c)
+
+
+
+
+# program to calculate square
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+# print(type(a), " ", type(b))
+# print("The Addition value is:", a + b)
+
+
+
+
+
+# Strings
+
+name = "Akhil"
+surname = "Upadhyay"
+print(name + "", surname)
+print(name[0])
+print(len(name))     # To find length of variable...
+
+
