6c40015fd1364e56b3bddf40cae875ff7c4dacf1
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-28 03:20:40 +0530

    Update file

--- hello.py
+++ hello.py
@@ -1188,10 +1188,10 @@
 
 
 
-for i in range(2, 21):
-    with open(f"Tables/Multiply_Table_of_{i}.txt", 'w') as f:
-        for j in range(1, 11):
-            f.write(f"{i} X {j} = {i * j}")
-            if j!=10:
-                f.write("\n")
+# for i in range(2, 21):
+#     with open(f"Tables/Multiply_Table_of_{i}.txt", 'w') as f:
+#         for j in range(1, 11):
+#             f.write(f"{i} X {j} = {i * j}")
+#             if j!=10:
+#                 f.write("\n")
     
