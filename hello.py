15a0059fe192665010e576422a8fcd863e0ea3d9
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-28 03:20:09 +0530

    Update file

--- hello.py
+++ hello.py
@@ -1180,3 +1180,18 @@
 #                 print("Updated Score is: " + scoreOfNow)
 #         else:
 #             print("New score is not more than high score which is:", hiscore)
+
+
+
+
+
+
+
+
+for i in range(2, 21):
+    with open(f"Tables/Multiply_Table_of_{i}.txt", 'w') as f:
+        for j in range(1, 11):
+            f.write(f"{i} X {j} = {i * j}")
+            if j!=10:
+                f.write("\n")
+    
