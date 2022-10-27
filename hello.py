cb8c56e7675f60f1219286e8102c9b7a063ab92c
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-27 19:07:46 +0530

    Update file

--- hello.py
+++ hello.py
@@ -787,11 +787,24 @@
 
 
 # n = int(input("Enter num: "))
-# for i in range(1, n + 1):
-#     for j in range(1, n + 1):
-#         if j <= i:
+# for i in range(0, n):
+#     for j in range(i + 1):
+#         # if j <= i:
 #             print("* ", end="")
 #     print("\r")
+
+
+
+
+
+n = int(input("Enter num: "))
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i==1 or i==n or j==1 or j==n:
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    print("\r")
 
 
 
