37bc29dc6c2d3c9dd67822c65d3157cb9d06ea04
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-27 23:27:49 +0530

    Update file

--- hello.py
+++ hello.py
@@ -1003,3 +1003,53 @@
  
 
 
+
+
+
+# Snake Water and Gun Game...
+
+import random
+
+def gameWin(comp, you):
+    if comp == you:
+        return None
+    elif comp == 's':
+        if you == 'w':
+            return False
+        elif you == 'g':
+            return True
+    elif comp == 'w':
+        if you == 'g':
+            return False
+        elif you == 's':
+            return True
+    elif comp == 'g':
+        if you == 's':
+            return False
+        elif you == 'w':
+            return True
+    
+print("Comp Turn: Computer already choosed..")
+randNo = random.randint(1,3)
+
+if randNo == 1:
+    comp = 's'
+elif randNo == 2:
+    comp = 'w'
+elif randNo == 3:
+    comp = 'g'
+
+you = input("Your Turn: Snake(s), Water(w), or Gun(g): ")
+
+a = gameWin(comp, you)
+
+print(f"computer choose {comp}")
+print(f"You choose {you}")
+
+
+if a == None:
+    print("Ohh! Game tie")
+elif a:
+    print("Congratulatios! You win!")
+else:
+    print("Sorry! You loose!")
