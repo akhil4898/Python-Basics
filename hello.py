5ce80b5650b814b9723b521b0063aabfe94d5eb2
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-27 11:41:10 +0530

    Update file

--- hello.py
+++ hello.py
@@ -606,21 +606,21 @@
 
 
 
-marks = int(input("Enter marks: "))
-if marks > 90:
-    grade = "Ex"
-elif marks > 80:
-    grade = "A"
-elif marks > 70:
-    grade = "B"
-elif marks > 60:
-    grade = "C"
-elif marks > 50:
-    grade = "D"
-else:
-    grade = "Fail"
+# marks = int(input("Enter marks: "))
+# if marks > 90:
+#     grade = "Ex"
+# elif marks > 80:
+#     grade = "A"
+# elif marks > 70:
+#     grade = "B"
+# elif marks > 60:
+#     grade = "C"
+# elif marks > 50:
+#     grade = "D"
+# else:
+#     grade = "Fail"
 
-print("Your grade is: ", grade)
+# print("Your grade is: ", grade)
 
 
 
