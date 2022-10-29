d79c97e19309152118c5031100e4e57bcbf6b92c
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-29 12:20:25 +0530

    Update file

--- learnFunction.py
+++ learnFunction.py
@@ -54,8 +54,50 @@
 # Docstring = The first string after the function is called the Document string
 # Syntax: print(function_name.__doc__)
 
+# def toDescribe():
+#     '''This is the funtion to describe about the function... What it does...'''
 
-def toDescribe():
-    '''This is the funtion to describe about the function... What it does...'''
+# print(toDescribe.__doc__)                                           # Docstring...
 
-print(toDescribe.__doc__)                                           # Docstring...
+
+
+
+
+
+
+# Anonymous functions = an anonymous function means that a function is without a name. And 
+                        #  the lambda keyword is used to create anonymous functions
+
+# cube = lambda n : n * n * n                                      # Anonymous functions
+# print(cube(7))
+
+
+
+
+
+
+
+# Function within Functions or nested function or inner function...
+
+
+# def fun():
+#     s = "This is just for fun()..."
+#     print("done for fun()...")
+
+#     def fun2():
+#         print(s)
+#         t = "This is just for fun2()..."
+#         print("donefor fun2()..")
+
+#         def fun3():
+#             print(t)
+#             print("done for fun3()...")
+
+#         fun3()
+
+#     fun2()
+
+# fun()
+
+
+
