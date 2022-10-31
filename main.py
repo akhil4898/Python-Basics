ccd7ea258753363be1cf35a87576627fba7be7f0
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2022-10-31 11:45:12 +0530

    Update file

---
+++ main.py
@@ -0,0 +1,11 @@
+a = int(input("Enter num1: "))
+b = int(input("Enter num2: "))
+c = int(input("Enter num3: "))
+
+sum = a + \
+    b + \
+    c                                       # Multiline statement denote that line should continue... 
+
+print("the sum is:", sum)
+
+
